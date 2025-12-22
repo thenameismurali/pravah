c55c01db92b864d0e916ce65e1a626be59f2a7db
Murali <kodimurali3335@gmail.com>
2025-12-23 00:33:34 +0530

    Fix CORS for Netlify subdomains3

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+
+const app = express(); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const app = express(); 
 const cors = require("cors");
 
 
