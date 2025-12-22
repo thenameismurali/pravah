c97bfa8ba4cf4b80b8c24b8ab2fac47ff3e817dd
Murali <kodimurali3335@gmail.com>
2025-12-23 00:19:36 +0530

    Fix CORS for Netlify subdomains

--- server.js
+++ server.js
@@ -10,11 +10,17 @@ const adminRoute =require("./routes/admin");
 const eventRoute = require("./routes/event");
 app.use(
   cors({
-    origin: [
-      "https://taupe-banoffee-9b6829.netlify.app",
-      "https://strong-praline-19bdfb.netlify.app",
-      "https://elegant-belekoy-b190c7.netlify.app"
-    ],
+    origin: (origin, callback) => {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      // allow all Netlify domains
+      if (origin.endsWith(".netlify.app")) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   })
 );
